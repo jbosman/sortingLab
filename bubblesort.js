9ad879ece24e56c1ba55ff0d79c004027fdcb76f
Joe Bosman <joseph.a.bosman@gmail.com>
2016-07-29 09:27:06 -0400

    Implemented bubble sort.

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,15 @@
 function bubbleSort(array){
-	return [];
+
+	for( var i = 0; i < array.length - 1; i++ ){
+		for( var j = 0; j < array.length - 1; j++ ){
+			var temp;
+			if( array[j] > array[ j + 1 ] ){
+				temp = array[j];
+				array[j] = array[j+1];
+				array[j+1] = temp;
+			}
+		}
+	}
+
+	return array;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,5 +1,18 @@
 describe('Bubble Sort', function() {
+
 	it('handles an empty array', function(){
 		expect( bubbleSort( [] )).toEqual( [] );
+	});
+
+	it('handles a single element array', function(){
+		expect( bubbleSort( [5] )).toEqual( [5] );
+	});
+
+	it('handles a multi-element array', function(){
+		expect( bubbleSort( [4,3,2,1] )).toEqual( [1,2,3,4] );
+	});
+
+	it('handles negative elements in the array', function(){
+		expect( bubbleSort( [4,3,2,1, -5] )).toEqual( [-5,1,2,3,4] );
 	});
 });
